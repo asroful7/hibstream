beeffec0408dafde65f397aa5c2676b7db99b17e
asroful7 <37543160+asroful7@users.noreply.github.com>
2022-04-12 16:21:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -57,8 +57,9 @@ gyroscope; picture-in-picture" allowfullscreen>
         if(getParameterByName('ib')){
             video = btoa(getParameterByName('ib'));
         }
-        var vidurl = document.write('url',eid);
-        document.write("<video controls id='video' src="+vidurl+"></video>");
+        
+
+        document.write("<video controls id='video' src="+eid+"></video>");
 
 
 function getembed(){
