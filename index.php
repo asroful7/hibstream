7d890ce3aa774f71dae3ff0f393da9f2ccc3c5d8
asroful7 <37543160+asroful7@users.noreply.github.com>
2022-04-12 17:08:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ gyroscope; picture-in-picture" allowfullscreen>
         if(getParameterByName('ib')){
             video = btoa(getParameterByName('ib'));
         }
-        var vidurl = "+video+"
+        var vidurl = "https://vk.com/"+video+"?alt=media&key="+apikey;
         document.write("<video controls id='video' src="+vidurl+"></video>");
 
 
