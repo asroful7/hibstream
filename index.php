ec58c9f57434fcdc9ca6c9d7c570be743659977e
asroful7 <37543160+asroful7@users.noreply.github.com>
2022-04-13 14:43:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ function getParameterByName(name, url = window.location.href) {
 			"autoHide": true
 		},
 		"htmlOnPauseBlock": {
-			"html": '<button class=\"embed\" onclick=\"getembed()\">HibStream</button>\n',
+			"html": '<button class=\"embed\" onclick=\"getembed()\">🔵HibStream</button>\n',
 			"height": null,
 			"width": null
 		},
