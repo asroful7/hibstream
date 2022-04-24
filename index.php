7889885ab30357765c29bcff2d5eed6e1ab3d671
asroful7 <37543160+asroful7@users.noreply.github.com>
2022-04-24 15:08:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Server Hibstream</title>
+	<title>Server Hibstream VIP</title>
 	<link rel="shortcut icon" href="https://bit.ly/samplefavicon" type="image/x-icon">
 
 </head>
